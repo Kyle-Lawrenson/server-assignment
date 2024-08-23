469c2365615d7679d38857d82a4620cd4f1ed525
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-08-23 11:34:08 +0100

    updated check password file and finsihed check token file

--- src/db/controllers/checkPassword.js
+++ src/db/controllers/checkPassword.js
@@ -1,34 +1,14 @@
-const jwt = require("jsonwebtoken");
-const User = require("../db/models/users");
- 
-async function checkToken(req,res,next) {
-    try {
-        //Creates encrypted password
-        const token = req.header("Authorization").replace("Bearer ","");
-        console.log(token);
-        const privateKey = process.env.JWT_KEY;
-        console.log(privateKey);
-        const decodedtoken = await jwt.verify(token,privateKey);
-        console.log(decodedtoken);
-        const userEmail = decodedtoken.email;
-        //Finds one user via email
-        const checkUserExists = await User.findOne({where:{email:userEmail}});
-        //If user doesnt exist throw a new error
-        if (checkUserExists === false) {
-            throw new Error("User no longer in database")
-        } else {
-            req.body.email = userEmail;
-            //continue to the next function
-            next()
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            message: "Token Check Failed",
-            errorMessage: error
-        })
-       
-    }
-}
- //Eports file to be used 
-module.exports = checkToken
+const bcrypt = require("bcrypt");
+
+//This is the encrypted password
+const hashedPassword =
+  "$2b$10$DX8o1waBy81OjK8xbTsVXOlYSAnBfppQGLSXDdAeglTUvkavrV/52";
+const plainTextPassword = "ThreeWordsHere";
+
+const checkPassword = async () => {
+  const result = await bcrypt.compare(plainTextPassword, hashedPassword);
+  console.log(result);
+};
+
+//This runs check password
+checkPassword();
--- src/middleware/checkPassword.js
+++ src/middleware/checkPassword.js
@@ -1,36 +1,34 @@
-const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../db/models/users");
- 
-const checkPassword= async (req,res,next) => {
+
+async function checkToken(req,res,next) {
     try {
-        //Checks Password to see if its correct
-        const plainTextPassword = req.body.password;
-        console.log(plainTextPassword);
-        const userDetails= await User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        console.log(userDetails);
-        const hashedPassword = userDetails.password;
-        console.log(hashedPassword);
-        
-        //Checks both the plain text password and the encrypted password to see if it matches and if it doesnt throw an error
-        const check = await bcrypt.compare(plainTextPassword, hashedPassword);
-        console.log(check);
-        if (check === true) {
-            next()
+        //Creates encrypted password
+        const token = req.header("Authorization").replace("Bearer ","");
+        console.log(token);
+        const privateKey = process.env.JWT_KEY;
+        console.log(privateKey);
+        const decodedtoken = await jwt.verify(token,privateKey);
+        console.log(decodedtoken);
+        const userEmail = decodedtoken.email;
+        //Finds one user via email
+        const checkUserExists = await User.findOne({where:{email:userEmail}});
+        //If user doesnt exist throw a new error
+        if (checkUserExists === false) {
+            throw new Error("User no longer in database")
         } else {
-            res.status(400).send("Password incorrect");
+            req.body.email = userEmail;
+            //continue to the next function
+            next()
         }
     } catch (error) {
         console.log(error);
         res.status(500).json({
-            message: "ooop something went wrong...",
+            message: "Token Check Failed",
             errorMessage: error
         })
        
-    }    
+    }
 }
- //Exports this file to be used in another
-module.exports = checkPassword
+ //Eports file to be used 
+module.exports = checkToken
